a181569a8429bb2d7f7e96e94031daec1d2bcfa5
MD. Aminul Haque Aome <aminul15-5281@diu.edu.bd>
2019-06-15 11:34:06 +0600

    Init DataBinding in LOginFragment & SignUpFragment

--- app/src/main/java/com/remotearth/fake_coder/chatapp/screens/fragments/LoginFragment.kt
+++ app/src/main/java/com/remotearth/fake_coder/chatapp/screens/fragments/LoginFragment.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.remotearth.fake_coder.chatapp.viewModels.LoginViewModel
 import com.remotearth.fake_coder.chatapp.R
+import com.remotearth.fake_coder.chatapp.databinding.LoginFragmentBinding
 import com.remotearth.fake_coder.chatapp.screens.fragments.base.BaseFragment
 
 class LoginFragment : BaseFragment() {
@@ -14,7 +16,13 @@ class LoginFragment : BaseFragment() {
     private lateinit var viewModel: LoginViewModel
 
     override fun initDataBinding(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.login_fragment, container, false)
+        val loginFragmentBinding: LoginFragmentBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.login_fragment,
+            container,
+            false
+        )
+        return loginFragmentBinding.root
     }
 
     override fun initialization() {
--- app/src/main/java/com/remotearth/fake_coder/chatapp/screens/fragments/SignUpFragment.kt
+++ app/src/main/java/com/remotearth/fake_coder/chatapp/screens/fragments/SignUpFragment.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.remotearth.fake_coder.chatapp.R
+import com.remotearth.fake_coder.chatapp.databinding.SignUpFragmentBinding
 import com.remotearth.fake_coder.chatapp.viewModels.SignUpViewModel
 import com.remotearth.fake_coder.chatapp.screens.fragments.base.BaseFragment
 
@@ -14,7 +16,13 @@ class SignUpFragment : BaseFragment() {
     private lateinit var viewModel: SignUpViewModel
 
     override fun initDataBinding(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.sign_up_fragment, container, false)
+        val signUpFragmentBinding: SignUpFragmentBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.sign_up_fragment,
+            container,
+            false
+        )
+        return signUpFragmentBinding.root
     }
 
     override fun initialization() {

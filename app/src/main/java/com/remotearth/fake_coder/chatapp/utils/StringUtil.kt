52d34f50d455f737b3fed09337df49cd68945592
MD. Aminul Haque Aome <aminul15-5281@diu.edu.bd>
2019-06-15 13:02:30 +0600

    Added null checker function

---
+++ app/src/main/java/com/remotearth/fake_coder/chatapp/utils/StringUtil.kt
@@ -0,0 +1,14 @@
+package com.remotearth.fake_coder.chatapp.utils
+
+object  StringUtil {
+
+    fun isNullOrEmpty(vararg fields: String?): Boolean {
+        for (field in fields) {
+            if (field == null || field.isEmpty()) {
+                return true
+            }
+        }
+        return false
+    }
+
+}

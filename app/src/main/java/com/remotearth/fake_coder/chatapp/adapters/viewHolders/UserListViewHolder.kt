102e9143a7b0ae1d508aff8d048a41718d68ee50
MD. Aminul Haque Aome <aminul15-5281@diu.edu.bd>
2019-06-17 16:45:17 +0600

    Created UserListAdapter

---
+++ app/src/main/java/com/remotearth/fake_coder/chatapp/adapters/viewHolders/UserListViewHolder.kt
@@ -0,0 +1,13 @@
+package com.remotearth.fake_coder.chatapp.adapters.viewHolders
+
+import androidx.recyclerview.widget.RecyclerView
+import com.remotearth.fake_coder.chatapp.User
+import com.remotearth.fake_coder.chatapp.databinding.UserItemBinding
+
+class UserListViewHolder(private val bindingView: UserItemBinding) : RecyclerView.ViewHolder(bindingView.root) {
+
+    fun bind(user: User) {
+        bindingView.user = user
+    }
+
+}

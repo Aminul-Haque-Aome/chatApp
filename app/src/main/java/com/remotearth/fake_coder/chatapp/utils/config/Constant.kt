90dd8d526ff89e310b577ecba4fe2275314d530a
MD. Aminul Haque Aome <aminul15-5281@diu.edu.bd>
2019-06-18 15:08:37 +0600

    Created DateTimeUtil

--- app/src/main/java/com/remotearth/fake_coder/chatapp/utils/config/Constant.kt
+++ app/src/main/java/com/remotearth/fake_coder/chatapp/utils/config/Constant.kt
@@ -1,9 +1,17 @@
 package com.remotearth.fake_coder.chatapp.utils.config
 
 object Constant {
+
     const val PICK_IMAGE_FROM_GALLERY_REQUEST_CODE = 69
 
     const val USER_TABLE = "Users"
     const val USER_FIELD_TOKEN = "token"
     const val USER_FIELD_IMAGE_URL = "profileImageUrl"
+
+    const val KEY_DATE_MONTH_YEAR = "dd-MM-yyyy"
+    const val KEY_DATE_MONTH_YEAR_HOUR_MINUTE_AM_PM = "dd-MM-yyyy h:mm a"
+    const val KEY_MONTH_YEAR = "MMMM,yy"
+    const val KEY_YEAR = "yyyy"
+    const val KEY_MONTH = "MMMM"
+    const val KEY_HOUR_MINUTE_AM_PM = "h:mm a"
 }

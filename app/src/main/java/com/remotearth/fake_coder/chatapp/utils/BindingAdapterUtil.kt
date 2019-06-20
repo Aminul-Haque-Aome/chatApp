063bbcf127a19925b3fcc1dbb8386f24dcf604fb
MD. Aminul Haque Aome <aminul15-5281@diu.edu.bd>
2019-06-20 11:57:40 +0600

    Loaded REceiver Image

--- app/src/main/java/com/remotearth/fake_coder/chatapp/utils/BindingAdapterUtil.kt
+++ app/src/main/java/com/remotearth/fake_coder/chatapp/utils/BindingAdapterUtil.kt
@@ -5,6 +5,10 @@ import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.remotearth.fake_coder.chatapp.R
+import com.remotearth.fake_coder.chatapp.User
+import com.remotearth.fake_coder.chatapp.callbacks.FireBaseRealTimeDataBaseCallback
+import com.remotearth.fake_coder.chatapp.services.FireBaseRealTimeDataBaseService
+import com.remotearth.fake_coder.chatapp.services.impls.FireBaseRealTimeDataBaseServiceImpl
 import com.remotearth.fake_coder.chatapp.utils.config.Constant
 import java.sql.Timestamp
 
@@ -24,5 +28,23 @@ object BindingAdapterUtil {
     @JvmStatic
     fun profileImageAttach(textView: TextView, timestamp: Long) {
         textView.text = DateTimeUtil.getDate(timestamp, Constant.DATE_MONTH_YEAR_HOUR_MINUTE_AM_PM)
+    }
+
+    @BindingAdapter("receiverImage")
+    @JvmStatic
+    fun getReceiverImage(imageView: ImageView, userId: String) {
+        FireBaseRealTimeDataBaseServiceImpl().retrieveUser(userId, object: FireBaseRealTimeDataBaseCallback.Retrieve {
+            override fun onRetrieveSuccess(user: User) {
+                Glide.with(imageView.context)
+                    .load(user.profileImageUrl)
+                    .centerCrop()
+                    .placeholder(R.drawable.ic_select_image)
+                    .into(imageView)
+            }
+
+            override fun onRetrieveFailed(error: String) {
+                imageView.setImageResource(R.drawable.ic_select_image)
+            }
+        })
     }
 }
